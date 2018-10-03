c56de24a685aac1e1cd8d072f6a284ef95e9a618
Michael Lee <michaellee2245@gmail.com>
2018-10-02 19:11:14 -0600

    adding first files

---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Product from './Component/Product/Product';
+
+class Dashboard extends Component {
+    
+    render() {
+        return(
+            <div>
+                Dashboard
+                <Product />
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
